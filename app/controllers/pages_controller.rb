2b224930ba400abdf1f7e6286342e66ff16ed858
Zhendong Yin <zhendongyin@brandeis.edu>
2022-10-30 16:13:25 -0400

    Add actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,35 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  before_action :load_customer, only: :payment_request_list
+  before_action :load_payment_request, only: :update
+
   def main
     @customers = Customer.all
   end
 
   def payment_request_list
+    @payment_requests = @customer&.payment_requests.order(:id)
   end
 
+
   def update
+    if @payment_request.update(payment_request_params)
+      # update
+    end
+  end
+
+  private
+
+  def load_customer
+    @customer = Customer.find_by(id: params[:customer_id])
+  end
+
+  def load_payment_request
+    @payment_request = PaymentRequest.find_by(id: params[:id])
+  end
+
+  def payment_request_params
+    params.require(:payment_request).permit(:status)
   end
 end
