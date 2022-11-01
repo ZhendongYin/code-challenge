1a3db5855c22ecc32b0c7181609b09f659511439
Zhendong Yin <zhendongyin@brandeis.edu>
2022-11-01 02:23:32 -0400

    Improve main page

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,6 +3,12 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @customer = customers(:jordan)
+    @payment_request = payment_requests(:jordans_payment)
+  end
+
   test "#main" do
     get root_path
 
@@ -10,16 +16,15 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "#payment_request_list" do
-    customer = Customer.first
-    get "/pages/payment_request_list/#{customer.id}"
+    get "/pages/payment_request_list/#{@customer.id}"
 
     assert_response :success
   end
 
   test "#update" do
-    pr = PaymentRequest.generated.first
-    put payment_request_path(pr), params: {payment_request: {status: :generated}}, xhr: true
-    pr.reload
-    assert_equal "generated", pr.status
+    put payment_request_path(@payment_request), params: {payment_request: {status: :collected}}, xhr: true
+    @payment_request.reload
+
+    assert_equal "collected", @payment_request.status
   end
 end
